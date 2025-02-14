602cf2db053caedd56756dfd464344074b9c4c10
Vishal2021verma <black2020panther@gmail.com>
2025-02-14 23:34:47 +0530

    image picked added4

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -8,11 +11,38 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final ImagePicker picker = ImagePicker();
+  XFile? image;
+
+  pickImage() async {
+    image = await picker.pickImage(source: ImageSource.gallery);
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Food Scan'),
+      ),
       body: Center(
-        child: Text("Food Scan"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 20,
+          children: [
+            image != null
+                ? SizedBox(
+                    width: 200,
+                    child: Image.file(File(image!.path), fit: BoxFit.fitWidth))
+                : Container(),
+            ElevatedButton(onPressed: pickImage, child: Text("Upload Image")),
+          ],
+        ),
       ),
     );
   }
